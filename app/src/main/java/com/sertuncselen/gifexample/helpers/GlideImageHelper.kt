1eb42500521bdc79f43e84813ba1cc8d3b60d363
cahitsertunc.selen <cahitsertunc.selen@kaleyazilim.com.tr>
2020-05-05 16:23:48 +0300

    refactoring

--- app/src/main/java/com/sertuncselen/gifexample/helpers/GlideImageHelper.kt
+++ app/src/main/java/com/sertuncselen/gifexample/helpers/GlideImageHelper.kt
@@ -1,8 +1,11 @@
 package com.sertuncselen.gifexample.helpers
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.target.ImageViewTarget
 import com.sertuncselen.gifexample.interfaces.IImageHelper
 
 class GlideImageHelper(context: Context) : IImageHelper {
@@ -13,5 +16,18 @@ class GlideImageHelper(context: Context) : IImageHelper {
         mGlideInstance
             .load(resourceId)
             .into(GifDrawableImageViewTarget(targetView, loopCount))
+    }
+}
+
+class GifDrawableImageViewTarget(view: ImageView?, private val mLoopCount: Int) :
+    ImageViewTarget<Drawable?>(view) {
+
+    override fun setResource(resource: Drawable?) {
+        if (resource is GifDrawable) {
+            if (mLoopCount > 0) {
+                resource.setLoopCount(mLoopCount)
+            }
+        }
+        view.setImageDrawable(resource)
     }
 }
